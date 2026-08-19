9a2f75837e09eff49f7f4e49ef99f9862e3f2288
Raunak Raj <raunakraj.it@gmail.com>
2026-08-19 09:37:50 +0530

    Time: 2 ms (15.81%) | Memory: 25.2 MB (5.56%) - LeetSync

---
+++ Array/283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                ans.push_back(nums[i]);
+            }
+        }
+        while(ans.size()<n){
+            ans.push_back(0);
+        }
+        nums=ans;
+    }
+};
