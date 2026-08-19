d2b7399640a16e4c3e6598059a2b3b876cb0e4ef
Raunak Raj <raunakraj.it@gmail.com>
2026-08-19 09:45:24 +0530

    Time: 107 ms (39.52%) | Memory: 95.2 MB (93.35%) - LeetSync

---
+++ Array/2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long  sum=0;
+        long long  maxi=0;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+            mpp[nums[i]]++;
+        }
+        if(mpp.size()==k){
+            maxi=sum;
+        }
+        for(int i=k;i<nums.size();i++){
+            mpp[nums[i-k]]--; //map me value gahtta hai aise 
+            if(mpp[nums[i-k]]==0){//ye condition se hm key (yani jo bhi value array me hoga usko hta denge )
+                mpp.erase(nums[i-k]);
+            }
+            mpp[nums[i]]++;//value  added next 
+            sum=sum-nums[i-k]+nums[i];
+            if(mpp.size()==k){//repeated value me size nhi badhta hai map me so ,check the condition 
+                maxi=max(maxi,sum);
+            }
+        }
+        return maxi;
+    }
+};
