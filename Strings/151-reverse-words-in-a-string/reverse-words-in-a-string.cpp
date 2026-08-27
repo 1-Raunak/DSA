cd9ba2d814c8ba62d157cc75a5a951669b32bba7
Raunak Raj <raunakraj.it@gmail.com>
2026-08-27 21:17:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (18.22%) - LeetSync

--- Strings/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ Strings/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        vector<string>words;
-        string word="";
-        for(int i=0;i<n;i++){
-            if(s[i]!=' '){
-                word+=s[i];
-            }
-            else if(!word.empty()){
-                //push_back the word into the words if it find " ";
-                words.push_back(word);
-            word="";
-            }
+        stringstream ss(s);
+        vector<string> words;
+        string word;
+
+        while (ss >> word) { //It will automatically ignore the spaces when using >>.
+            words.push_back(word);
         }
-        if(!word.empty()){
-             words.push_back(word);
+
+        reverse(words.begin(), words.end());
+
+        string ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (i > 0) ans += " ";
+            ans += words[i];
         }
-        reverse(words.begin(),words.end());
-        string result="";
-        for(int i=0;i<words.size();i++){
-            result+=words[i];
-            if(i<words.size()-1){
-                result+=" ";
-            }
-        }
-        return result;
+
+        return ans;
     }
 };
